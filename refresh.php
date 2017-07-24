d1116b55f4f63caf4312a19a5876f49f59391000
soybrubs <30419827+soybrubs@users.noreply.github.com>
2017-07-24 17:58:33 -0300

    Update refresh.php

--- refresh.php
+++ refresh.php
@@ -1,9 +1,9 @@
 <?php
 $url = 'https://graph.facebook.com/v2.9';
 $fields = array(
-            'id'=>urlencode('http://m-l-a-b-s.com'),
+            'id'=>urlencode('quem-sou-eu-na-gw.com'),
             'scrape'=>urlencode(true),
-            'access_token'=>"<coloque_seu_token_do_aplicativo_facebook_aqui>"
+            'access_token'=>"<322785598179500|DpkXV098JtwEosbrSkw8bcl6AXM>"
         );
 
 $fields_string = '';
